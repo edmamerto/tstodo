97a1f10a0d7af246bbe1acd5e026bd0f5b669a56
Ed Mamerto <edmamerto@gmail.com>
2019-02-12 00:02:26 -0500

    update todo

--- app.ts
+++ app.ts
@@ -3,6 +3,13 @@ interface Todo {
     completed?: boolean;
 }
 
+interface ITodoService {
+	add(todo: Todo): Todo;
+	delete(todoId: number): void;
+	getAll(): Todo[]; //returns array of todos
+	getById(todoId: number): Todo; //returns single todo
+}
+
 var todo= <Todo>{
 	name: "hello",
 };
